d781f6b00081abd775ead7835e8dd1ff97c51f1e
Mathew Sunday <sunnycity2007@yahoo.com>
2023-04-27 10:48:25 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,9 +9,10 @@
  *
  * Return: address of the new element, or NULL if it failed
  */
-list_t *add_node_end(list_t **head, const char *str)
+	list_t *add_node_end(list_t **head, const char *str)
 {
-	:wqlist_t *new;
+	list_t *new;
+
 	list_t *temp = *head;
 	unsigned int len = 0;
 
@@ -25,12 +26,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
- 
+
 	if (*head == NULL)
 	{
 	*head = new;
 	return (new);
- 	}
+	}
 
 	while (temp->next)
 	temp = temp->next;
